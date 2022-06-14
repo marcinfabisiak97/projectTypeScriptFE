e30c1d52325e0c462a6d7d8d9fc05761135a02ab
marcinfabisiak97 <marcinfab1@o2.pl>
2022-06-14 02:16:43 +0200

    making axios request

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/products")
+      .then((response) => {
+        setPost(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!post) return null;
   return (
-    <div className="App">
-      <h1>Helllo</h1>
-    </div>
+    <Fragment>
+      {post.map((el) => (
+        <div style={{ backgroundColor: el.color }}>
+          <h1>{el.id}</h1>
+          <p>{el.name}</p>
+          <p>{el.year}</p>
+          <p>{el.color}</p>
+          <p>{el.pantone_value}</p>
+        </div>
+      ))}
+    </Fragment>
   );
 }
 
